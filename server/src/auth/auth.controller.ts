a23d0e61b0365be2bffef949f0d67602e023bc9b
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-06-30 02:27:16 +0300

    123

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -32,7 +32,6 @@ export class AuthController {
     return { access_token: token };
   }
 
-  @Public()
   @Post('/create')
   @UsePipes(new ValidationPipe({ transform: true }))
   async createUser(@Body() data: LoginDto) {
